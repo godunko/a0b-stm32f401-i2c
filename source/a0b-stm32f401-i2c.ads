dc85bf097a8c7c93522b934a2a272044d00490b5
Vadim Godunko <vgodunko@gmail.com>
2024-10-01 10:16:13 +0400

    Add package renaming for convenience.

---
+++ source/a0b-stm32f401-i2c.ads
@@ -0,0 +1,11 @@
+--
+--  Copyright (C) 2024, Vadim Godunko <vgodunko@gmail.com>
+--
+--  SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+--
+
+pragma Restrictions (No_Elaboration_Code);
+
+with A0B.I2C.STM32F401_I2C;
+
+package A0B.STM32F401.I2C renames A0B.I2C.STM32F401_I2C;
